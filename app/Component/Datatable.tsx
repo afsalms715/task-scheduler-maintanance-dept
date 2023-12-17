70a3bf4a35f0d04cfe7b2e806de21771f203de9c
afsalms715 <afsalms715@gmail.com>
2023-12-17 15:14:59 +0300

    api data fetch done | datatable data update implimented

--- app/Component/Datatable.tsx
+++ app/Component/Datatable.tsx
@@ -1,4 +1,4 @@
-import React,{useEffect, useRef} from 'react'
+import React,{useEffect, useRef,useState} from 'react'
 import DataTable from 'datatables.net-dt';
 import 'datatables.net-buttons'
 import * as $ from 'jquery';
@@ -13,41 +13,84 @@ import "datatables.net-buttons/js/buttons.html5.js"
 import "datatables.net-buttons/js/buttons.print.js"
 import "datatables.net-dt/css/jquery.dataTables.min.css"
 
-const Datatable = () => {
+type propModel={
+    reportData:any;
+    dtStatus:boolean;
+    setDtStatus:any;
+    dtState:any;
+    setDtState:any;
+}
+
+const Datatable:React.FC<propModel> = ({reportData,dtStatus,setDtStatus,dtState,setDtState}) => {
+    console.log(reportData)
     const tableRef:any=useRef('table')
-    let table:any;
-    useEffect(()=>{
-        if(!table){
+    const dataTableRef = useRef(null);
+
+    const[tableState,setTableState]=useState(false);
+    let table:any=dtState
+    const DTinitialization=(reportData:any)=>{ 
+            console.log("table declaring")
             table = new DataTable(tableRef.current, {
                 dom: 'Bfrtip',
                 processing: true,
-                data:[],
+                data:reportData,
+                columns: [
+                    // { data: 'id' },
+                    // { data: 'name' },
+                    // { data: 'desig' },
+                    // { data: 'workDate' },
+                    // { data: 'workId' },
+                    // { data: 'startTime' },
+                    // { data: 'endTime' },
+                    // { data: 'workTime' },
+                    // { data: 'workLocation' },
+                    // { data: 'workDetails' },
+                    { title: 'id', data: 'id' },
+                    { title: 'name', data: 'name' },           
+                    { title: 'desig', data: 'desig' },
+                    { title: 'workDate', data: 'workDate' },
+                    { title: 'workId', data: 'workId' },
+                    { title: 'startTime', data: 'startTime' },           
+                    { title: 'endTime', data: 'endTime' },
+                    { title: 'workTime', data: 'workTime' },
+                    { title: 'workLocation', data: 'workLocation' },
+                    { title: 'workDetails', data: 'workDetails' }
+                ],
                 buttons: [
                     'copy','csv','print'
                 ],
-        });}
-        table.clear();
-        table.rows.add([]);
-        table.draw();
-    },[])
+        }
+        );
+        setDtState(table)
+    }
     
+    useEffect(()=>{
+        console.log(table)
+        if(!table && !dtStatus){
+            DTinitialization(reportData)
+            setDtStatus(true);
+        }else{
+            table.clear();
+            table.rows.add(reportData);
+            table.draw();
+        }
+    },[reportData])
+
   return (
     <div className='mt-4 m-1 w-full'>
         <table className='w-full border' ref={tableRef}>
-            <thead>
+            <thead className='w-full'>
                 <tr>
-                    <th>date</th>
-                    <th>Employee</th>
-                    <th>Work</th>
-                    <th>date</th>
-                    <th>Employee</th>
-                    <th>Work</th>
-                    <th>date</th>
-                    <th>Employee</th>
-                    <th>Work</th>
-                    <th>date</th>
-                    <th>Employee</th>
-                    <th>Work</th>
+                    <th>Emp ID</th>
+                    <th>Employee Name</th>
+                    <th>Designation</th>
+                    <th>Work Date</th>
+                    <th>Work ID</th>
+                    <th>Start Time</th>
+                    <th>End Time</th>
+                    <th>Work Time</th>
+                    <th>Work Location</th>
+                    <th>Work Details</th>
                 </tr>
             </thead>
         </table>
--- app/Component/WorkReport.tsx
+++ app/Component/WorkReport.tsx
@@ -6,13 +6,28 @@ type propModel={
 }
 
 const WorkReport:React.FC<propModel> = ({employees}) => {
-    const[formData,setFormData]=useState({})
+    const[formData,setFormData]=useState({fromDate:0,toDate:0,employeeId:0,location:0})
+    const[reportData,setReportData]=useState([])
+    const[dtStatus,setDtStatus]=useState(false)
+
+    //testing
+    const[dtState,setDtState]=useState();
+
     const handleChnages=(e:any)=>{
         setFormData({...formData,[e.target.name]:e.target.value})
     }
+
     const reportRun=()=>{
         console.log(formData)
+        if(formData.fromDate!=0 && formData.toDate!=0){
+            fetch(`https://localhost:44376/api/WorkScheduler/MNTC_work_report?Empid=${formData.employeeId}&FromDate=${formData.fromDate}&Location=${formData.location}&ToDate=${formData.toDate}`)
+            .then((responce:any)=>responce.json()).then((data)=>{
+                console.log(data)
+                setReportData(data)
+            })
+        }
     }
+
   return (
     <div className='h-[100vh] w-[90%]'>
         <div className='m-2'>
@@ -27,10 +42,10 @@ const WorkReport:React.FC<propModel> = ({employees}) => {
                 </div>
                 <div className='ml-1'>
                     <label className='text-sm block'>Employee</label>
-                    <select name='employee' onChange={(e)=>handleChnages(e)} className='border w-60 p-[5px] text-sm rounded-md'>
+                    <select name='employeeId' onChange={(e)=>handleChnages(e)} className='border w-60 p-[5px] text-sm rounded-md'>
                         <option value="">select</option>
                         {employees.map((item:any)=>{
-                            return <option>{item.name}</option>
+                            return <option value={item.id}>{item.name}</option>
                         })}
                     </select>    
                 </div>
@@ -53,7 +68,7 @@ const WorkReport:React.FC<propModel> = ({employees}) => {
                 </div>
             </div>
         </div>
-        {<Datatable/>}
+        {<Datatable reportData={reportData} dtStatus={dtStatus} setDtStatus={setDtStatus} setDtState={setDtState} dtState={dtState}/>}
     </div>
   )
 }
